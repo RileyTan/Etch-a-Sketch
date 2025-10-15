7052bba4c281784193ad31f40abeb8f23f21e0fb
Riley Tan <xuanyun.tan@gmail.com>
2025-10-15 16:28:55 +0800

    3 flex row children version

---
+++ logic.js
@@ -0,0 +1,27 @@
+const containerOfSquares = document.querySelector("#container")
+
+const SQUARES = 16 
+
+// create 16 rows
+for (let squares = 0; squares < SQUARES; squares++) {
+    // create 1 row
+    const row = document.createElement("div")
+    row.classList.add("row")
+    for (let col = 0; col < SQUARES; col++) {
+        const cell = document.createElement("div")
+        cell.classList.add("column")
+        row.insertAdjacentElement("beforeend", cell)
+    }
+    // add 1 completed row to the grid
+    containerOfSquares.insertAdjacentElement("beforeend", row)
+}
+
+const cells = document.querySelectorAll(".column")
+cells.forEach(cell => {
+    cell.addEventListener("mouseenter", event => {
+        const randomColor = `rgb(${Math.floor(Math.random() * 256)}, 
+                             ${Math.floor(Math.random() * 256)}, 
+                             ${Math.floor(Math.random() * 256)})`
+        event.currentTarget.style.backgroundColor = randomColor
+    })
+})
