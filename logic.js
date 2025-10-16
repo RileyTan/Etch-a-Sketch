5460439339d9b679f0d3177227b0dd6c760c83a3
Riley Tan <xuanyun.tan@gmail.com>
2025-10-16 16:06:32 +0800

    Before shifting of row
    Making the button row separate from the grid

--- logic.js
+++ logic.js
@@ -1,20 +1,22 @@
 const containerOfSquares = document.querySelector("#container")
 
-const SQUARES = 16 
-
-// create 16 rows
-for (let squares = 0; squares < SQUARES; squares++) {
-    // create 1 row
-    const row = document.createElement("div")
-    row.classList.add("row")
-    for (let col = 0; col < SQUARES; col++) {
-        const cell = document.createElement("div")
-        cell.classList.add("column")
-        row.insertAdjacentElement("beforeend", cell)
+const makeGrid = (SQUARES) => {
+    // create 16 rows
+    for (let squares = 0; squares < SQUARES; squares++) {
+        // create 1 row
+        const row = document.createElement("div")
+        row.classList.add("row")
+        for (let col = 0; col < SQUARES; col++) {
+            const cell = document.createElement("div")
+            cell.classList.add("column")
+            row.insertAdjacentElement("beforeend", cell)
+        }
+        // add 1 completed row to the grid
+        containerOfSquares.insertAdjacentElement("beforeend", row)
     }
-    // add 1 completed row to the grid
-    containerOfSquares.insertAdjacentElement("beforeend", row)
 }
+
+makeGrid(16) // always 16 squares upon refresh
 
 // code to make hover effect 
 const cells = document.querySelectorAll(".column")
@@ -30,5 +32,10 @@ cells.forEach(cell => {
 // code to prompt user for number of squares per side
 const setGrid = document.querySelector("#set-grid")
 setGrid.addEventListener("click", () => {
-    prompt("")
+    let squaresWanted = prompt("Choose number of squares per side from 16 to 100")
+    while ( squaresWanted > 100 || squaresWanted < 16 || isNaN(squaresWanted)) {
+        squaresWanted = prompt("Choose number of squares from 16 to 100")
+    }
+    containerOfSquares.innerHTML = ""
+    makeGrid(squaresWanted)
 })
